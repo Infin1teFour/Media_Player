3f415f1de5dd388789c16bede2e5afa7cc293779
Yla07 <bastianwic07@wp.pl>
2023-12-05 20:29:36 +0100

    unorganizing

--- player.py
+++ player.py
@@ -25,7 +25,7 @@ root = tk.Tk()
 root.resizable(0,0)
 root.title("Media Player")
 root.config(bg="#717291")
-root.iconbitmap("grafiki/icon.ico")
+root.iconbitmap("icon.ico")
 
 # Setting up fonts
 pyglet.options['win32_gdi_font'] = True
